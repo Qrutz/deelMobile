26f4b3a72539e5f8e366effbdff5b9ceb51b2467
Johan Sandgren <gussanjoba@student.gu.se>
2025-01-17 14:45:27 +0100

    Fix formatting in DealBuilderScreen by adjusting SafeAreaView spacing

--- app/(home)/proposeTradeModal.tsx
+++ app/(home)/proposeTradeModal.tsx
@@ -112,7 +112,7 @@ export default function DealBuilderScreen() {
     }
 
     return (
-        <SafeAreaView style={styles.safeArea} >
+        <SafeAreaView style={styles.safeArea}  >
             {/* Possibly a translucent status bar at the top */}
             < ScrollView contentContainerStyle={styles.scrollContent} >
                 {/* Top header */}
